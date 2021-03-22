7ca25eaf1e9033d3d70260c5c170300ff1fe4e7a
Dmytro <krasilnikov.dmytro@gmail.com>
2021-03-22 16:41:23 +0200

    Add new cata

---
+++ 7kyu/Easy Line.js
@@ -0,0 +1,15 @@
+// In the drawing below we have a part of the Pascal's triangle, horizontal lines are numbered from zero (top).
+
+// We want to calculate the sum of the squares of the binomial coefficients on a given horizontal line with a function called easyline (or easyLine or easy-line).
+
+// Can you write a program which calculate easyline(n) where n is the horizontal line number?
+
+// The function will take n (with: n>= 0) as parameter and will return the sum of the squares of the binomial coefficients on line n.
+
+function easyLine(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= (n + i) / i;
+  }
+  return Math.round(Math.log(result));
+}
